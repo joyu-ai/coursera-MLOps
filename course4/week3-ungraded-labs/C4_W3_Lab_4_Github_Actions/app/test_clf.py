1a7d76aea235a18ddcff52192915dea9a73ef9c2
Jonelle Yu <joyu@MacBook-Pro.local>
2023-04-20 10:29:40 -0700

    Adding new classifier with scaling 2

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
@@ -5,13 +5,13 @@ from sklearn.preprocessing import StandardScaler
 
 
 def test_pipeline_and_scaler():
-        
+
     isPipeline = isinstance(clf, Pipeline)
     assert isPipeline
 
     if isPipeline:
-	firstStep = [v for v in clf.named_steps.values()][0]
-	assert isinstance(firstStep, StandardScaler)
+        firstStep = [v for v in clf.named_steps.values()][0]
+        assert isinstance(firstStep, StandardScaler)
 
 
 def test_accuracy():
